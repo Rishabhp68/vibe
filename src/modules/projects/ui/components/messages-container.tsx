972a61a137ffb28dfb74b75ebf61753c92211777
Rishabhp68 <rishabhpalohia68@gmail.com>
2025-08-01 00:09:02 +0530

    bug fiexes expect date error handling in the usage page.

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -27,8 +27,7 @@ export const MessagesContainer = ({
     const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId: projectId,
     }, {
-        // TODO: Temprory live message update
-        refetchInterval: 5000,
+        refetchInterval: 2000,
     }));
 
     useEffect(() => {
